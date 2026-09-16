75499d1d6d5018a3d0e64b45ede7c387e13a8d5d
edcong1 <gbg6mzchng@privaterelay.appleid.com>
2026-09-16 19:17:53 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// Configuración de El Gran Pastor V4 - Supabase
+window.EGP_SUPABASE_URL = "https://zsowsizbvdqesfxqkqdz.supabase.co";
+window.EGP_SUPABASE_ANON_KEY = "sb_publishable_TSOpipR2g5GSGUiZcrSUIw_xNshsdoA";
